584919f67b525271dbc7831763c1419f9598ffe1
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-02-08 18:51:26 -0600

    burger.js File Updated.

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,18 @@
-const orm = require("./config/orm.js");
+const orm = require("../config/orm.js");
 
 
-const burger = {};
+const burger = {
+    all(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
+    },
+
+    create(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, (res) => cb(res));
+    },
+
+    devour(objColsVals, condition, cb) {
+        orm.updateOne('burgers', objColsVals, condition, (res) => cb(res));
+    }
+};
 
 module.exports = burger;
