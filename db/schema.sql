0e81813acf8f109cb38e53409bf8b59f3af3b6e4
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-02-09 19:26:09 -0600

    Styling Mostly Complete.

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,5 @@ USE burgers_db;
 CREATE TABLE burgers(
     id int auto_increment not null primary key,
     burger_name varchar(50) not null,
-    devoured boolean
+    devoured boolean default false
 );
