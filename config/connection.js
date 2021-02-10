4de89cf2d654a2fe45c890ab31137f413e138e4f
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-02-09 21:26:04 -0600

    Setting Up JAWSDB.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "85716",
-    database: "burgers_db",
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "85716",
+        database: "burgers_db",
+    });
+}
+
 
 module.exports = connection;
